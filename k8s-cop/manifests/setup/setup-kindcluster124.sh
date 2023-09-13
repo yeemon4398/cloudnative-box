3f0a2df5dc67490f3c9e12db1c1a969f1650574f
Yee Mon <40330106+yeemon4398@users.noreply.github.com>
2023-09-13 09:34:01 +0630

    Update setup-kindcluster124.sh

--- k8s-cop/manifests/setup/setup-kindcluster124.sh
+++ k8s-cop/manifests/setup/setup-kindcluster124.sh
@@ -3,11 +3,11 @@
 sudo sysctl fs.inotify.max_user_watches=524288
 sudo sysctl fs.inotify.max_user_instances=512
 
-kind create cluster --config ../kind-cluster/kindconfig-v1244.yaml      # Create kind cluster 124
+kind create cluster --config ../config-files/kindconfig-v1244.yaml      # Create kind cluster 124
 sleep 1
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml --context kind-124
 sleep 120
-kubectl create -f ../kind-cluster/metallb-v0139-ipaddress-pool-2.yaml --context kind-124
+kubectl create -f ../config-files/metallb-v0139-ipaddress-pool-2.yaml --context kind-124
 sleep 0.5
 kubectl config rename-context kind-124 124      # kubectl config rename-context kind-ambient ambient
 watch kubectl get pods -A --context 124
